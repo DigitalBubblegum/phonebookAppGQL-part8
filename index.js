b1bc4484537002e154067d1cc570ce9a42836d3d
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-04 14:55:24 +0530

    -added mongoose

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const {v1: uuid} = require('uuid')
 const {ApolloServer} = require('@apollo/server')
 const {startStandaloneServer} = require('@apollo/server/standalone')
 const { GraphQLError } = require('graphql')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const Person = require('./models/person')
+require('dotenv').config()
+const MONGODB_URI = process.env.MONGODB_URI
+console.log('connecting to', MONGODB_URI)
+mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI)
+.then(()=>{
+  console.log('connected to MONGODB')
+})
+.catch((error) => {
+  console.log('error connection to MONGODB',error.message)
+})
+
 let persons = [
   {
     name: "Arto Hellas",
@@ -63,17 +79,14 @@ type Mutation {
 `
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
-    allPersons: (root, args) => {
+    personCount: async () => Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
     if (!args.phone) {
-      return persons
+      return Person.find({})
     }
-    const byPhone = (person) =>
-      args.phone === 'YES' ? person.phone : !person.phone
-    return persons.filter(byPhone)
-  },
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+    return Person.find({ phone: { $exists: args.phone === 'YES' } })
+    },
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
   },
 
   Person: {
@@ -85,29 +98,15 @@ const resolvers = {
     }
   },
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find(p => p.name === args.name)) {
-        throw new GraphQLError('Name must be unique', {
-          extensions: {
-            code: 'BAD_USER_INPUT',
-            invalidArgs: args.name
-          }
-        })
-      }
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
-      return person
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args })
+      return person.save()
     },
-    editNumber: (root, args) => {
-    const person = persons.find(p => p.name === args.name)
-    if (!person) {
-      return null
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+      return person.save()
     }
-
-    const updatedPerson = { ...person, phone: args.phone }
-    persons = persons.map(p => p.name === args.name ? updatedPerson : p)
-    return updatedPerson
-  }
   }
 }
 
